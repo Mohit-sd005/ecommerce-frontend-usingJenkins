c18f66f2b15e68db5301f992352f175063e393fb
Mohit-sd005 <mail2moht@gmail.com>
2025-08-02 00:02:04 +0530

    final-commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecommerce-frontend-usingJenkins">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
