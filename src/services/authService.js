1ba40ff345da9d92a2dc152bee258ef3556032ae
Mohit-sd005 <mail2moht@gmail.com>
2025-08-03 17:40:24 +0530

    finalc

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:7099/auth";
+// const API_URL = "http://localhost:7099/auth";
+const API_URL = "http://localhost:8099/back1/auth";
+
 
 
 export const login = async (username, password) => {
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // Use the correct base URL for cart operations
-const CART_API_URL = "http://localhost:7099/api/cart";
+// const CART_API_URL = "http://localhost:7099/api/cart";
+const CART_API_URL = "http://localhost:8099/back1/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${CART_API_URL}/add`, {
